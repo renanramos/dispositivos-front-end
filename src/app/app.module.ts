2e69f073549c9429457c02e17edfdd6b544edb6b
Renan Rodrigues Ramos <renan.ramos@inatel.br>
2021-02-17 14:20:56 -0300

    Adicionados arquivos de configuracao da aplicacao e do bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DeviceModule } from './device/device.module';
 import { HttpClientModule } from '@angular/common/http';
+import { AddButtonComponent } from './device/shared/components/add-button/add-button.component';
+import { DialogAlertComponent } from './device/shared/alert/dialog-alert/dialog-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddButtonComponent,
+    DialogAlertComponent
   ],
   imports: [
     BrowserModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot(),
     AppRoutingModule,
     DeviceModule,
     HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [DialogAlertComponent]
 })
 export class AppModule { }
