bfc71f271174cfe7f085e569a9955772f117224b
Renan Rodrigues Ramos <renan.ramos@inatel.br>
2021-02-17 14:19:27 -0300

    Ajustada aplicacao para reutilizacao de formulario na criacao ou edicao de device

--- src/app/device/device-form/device-form.component.ts
+++ src/app/device/device-form/device-form.component.ts
@@ -3,26 +3,34 @@ import { Device } from '../shared/device';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DeviceApiService } from '../device-api.service';
 import { Router } from '@angular/router';
+import { BsModalRef } from "ngx-bootstrap/modal";
+import { tap } from 'rxjs/operators';
 
 declare var $: any;
 
 @Component({
-  selector: 'app-device-form',
-  templateUrl: './device-form.component.html',
-  styleUrls: ['./device-form.component.css']
+  selector: "app-device-form",
+  templateUrl: "./device-form.component.html",
+  styleUrls: ["./device-form.component.css"],
 })
 export class DeviceFormComponent implements OnInit {
-
   deviceForm: FormGroup;
+  device: Device;
 
   constructor(
     private formBuilder: FormBuilder,
     private deviceService: DeviceApiService,
-    private route: Router
-  ) { }
+    private route: Router,
+    private deviceFormModalRef: BsModalRef<DeviceFormComponent>
+  ) {}
 
   ngOnInit() {
-    this.deviceFormInit();   
+    this.setDeviceValueToForm();
+    this.deviceFormInit();
+  }
+
+  setDeviceValueToForm() {
+    this.device = this.device ? this.device : new Device();
   }
 
   /**
@@ -30,13 +38,28 @@ export class DeviceFormComponent implements OnInit {
    */
   deviceFormInit() {
     this.deviceForm = this.formBuilder.group({
-      id: [''],
-      modelo: ['', Validators.required],
-      fabricante: ['', Validators.required],
-      capacidadeArmazenamento: ['', Validators.required],
-      tamanhoTela: ['', Validators.required],
-      versaoSO: ['', Validators.required],
-    })
+      id: [this.device.device_id],
+      modelo: [
+        this.device.device_modelo,
+        Validators.required,
+      ],
+      fabricante: [
+        this.device.device_fabricante,
+        Validators.required,
+      ],
+      capacidadeArmazenamento: [
+        this.device.device_capacidade_armazenamento,
+        Validators.required,
+      ],
+      tamanhoTela: [
+        this.device.device_tamanho_tela,
+        Validators.required,
+      ],
+      versaoSO: [
+        this.device.device_versao_so,
+        Validators.required,
+      ],
+    });
   }
 
   get d() {
@@ -44,27 +67,73 @@ export class DeviceFormComponent implements OnInit {
   }
 
   onSubmit() {
-
-    if (this.validaForm()){
-      $('#modalFormInvalido').modal('show');      
-      return; 
+    if (this.validaForm()) {
+      // $("#modalFormInvalido").modal("show");
+      return;
     } else {
+
+      this.saveDevice();
+
       
-      let device: Device = {
+    }
+  }
+
+  saveDevice() {
+    const device: Device = this.getDeviceValuesFromForm();
+    device['device_id'] ?
+      this.updateDevice(device) :
+      this.createDevice(device);
+  }
+
+  createDevice(device: Device) {
+
+    const createdDevice = {
+      next: (response) => {
+        if (response) {
+          this.deviceFormModalRef.hide();
+        }
+      },
+      error: (response) => {
+        console.log(response);
+      }
+    }
+
+    this.deviceService.createNewDevice(device)
+      .pipe(tap(createdDevice))
+      .toPromise()
+      .then(() => true)
+      .catch(() => false);
+  }
+  
+  updateDevice(device: Device) {
+
+    const updatedDevice = {
+      next: (response) => {
+        if (response.status === 200) {
+          this.deviceFormModalRef.hide();
+        }
+      },
+      error: (response) => {
+        console.log(response);
+      }
+    }
+
+    this.deviceService.updateDevice(device)
+      .pipe(tap(updatedDevice))
+      .toPromise()
+      .then(() => true)
+      .catch(() => false);
+  }
+
+  getDeviceValuesFromForm(): Device {
+      return {
         device_id: this.d.id.value,
         device_modelo: this.d.modelo.value,
         device_fabricante: this.d.fabricante.value,
         device_capacidade_armazenamento: this.d.capacidadeArmazenamento.value,
         device_tamanho_tela: this.d.tamanhoTela.value,
-        device_versao_so: this.d.versaoSO.value
-      }    
-      
-      this.deviceService.createNewDevice(device).subscribe((response: any) => {
-        if ( response.status === 201){
-          this.route.navigateByUrl('/');
-        }
-      })
-    }
+        device_versao_so: this.d.versaoSO.value,
+      };
   }
 
   /**
@@ -74,4 +143,7 @@ export class DeviceFormComponent implements OnInit {
     return this.deviceForm.invalid;
   }
 
+  closeDialog() {
+    this.deviceFormModalRef.hide();
+  }
 }
--- src/app/device/device-list/device-list.component.ts
+++ src/app/device/device-list/device-list.component.ts
@@ -2,68 +2,91 @@ import { Component, OnInit } from '@angular/core';
 import { DeviceApiService } from '../device-api.service';
 import { Device } from '../shared/device';
 import { Router } from '@angular/router';
-import { pipe } from 'rxjs';
+import { tap, map } from "rxjs/operators";
+import { BsModalRef, BsModalService } from "ngx-bootstrap/modal";
+import { DeviceFormComponent } from "../device-form/device-form.component";
 
 declare var $: any;
 
 @Component({
-  selector: 'app-device-list',
-  templateUrl: './device-list.component.html',
-  styleUrls: ['./device-list.component.css']
+  selector: "app-device-list",
+  templateUrl: "./device-list.component.html",
+  styleUrls: ["./device-list.component.css"],
 })
 export class DeviceListComponent implements OnInit {
-
-  searchModelo = '';
+  searchModelo = "";
   devices: any[];
   device: Device;
   showAlert = false;
+  deviceFormModalRef: BsModalRef<DeviceFormComponent>;
 
   constructor(
     private deviceService: DeviceApiService,
-    private router: Router) { }
+    private modalService: BsModalService
+  ) {}
 
-  ngOnInit() {
-    this.loadDevices();    
+  async ngOnInit() {
+    await this.loadDevices();
   }
 
-  loadDevices() {    
-    this.deviceService.getAllDevices().subscribe((data: any) => {      
-      this.devices = data;
+  async loadDevices() {
+    const devicesReceived = {
+      next: (devices: Device[]) => {
+        if (devices.length) {
+          this.devices = devices;
+        }
+      },
+      error: (response) => {
+        console.log(response);
+      },
+    };
+
+    await this.deviceService
+      .getAllDevices()
+      .pipe(tap(devicesReceived))
+      .toPromise()
+      .then(() => true)
+      .catch(() => false);
+  }
+
+  editarDevice(device: Device) {
+    this.deviceFormModalRef = this.modalService.show(DeviceFormComponent, {
+      initialState: { device: device },
+      animated: true,
+      backdrop: "static",
     });
-  }
 
-  editarDevice(device: Device){
-    this.device = device;
-    $('#modalEditDevice').modal('show');
+    this.deviceFormModalRef.onHidden.subscribe(() => this.loadDevices());
   }
 
   modalExcluiDispositivo(device: Device) {
     this.device = device;
-    $('#excluirDevice').modal('show');    
+    $("#excluirDevice").modal("show");
   }
 
-  excluirDispositivo(device: Device){
+  excluirDispositivo(device: Device) {
     this.deviceService.deleteDevice(device).subscribe((response: any) => {
-      if (response.status === 200){
+      if (response.status === 200) {
         this.loadDevices();
       }
     });
   }
 
-  pesquisarModelo(){
-    this.deviceService.getByModelo(this.searchModelo).subscribe((response: any) => {
-      if (response.length > 0) {
-        this.devices = response;
-        this.showAlert = true;
-      } else {
-        $('#modalNaoEncontrado').modal('show');
-      }
-    })
-  }
-  
-  removeFiltroAlerta(){
-    this.loadDevices();
-    this.searchModelo = '';
+  pesquisarModelo() {
+    this.deviceService
+      .getByModelo(this.searchModelo)
+      .subscribe((response: any) => {
+        if (response.length > 0) {
+          this.devices = response;
+          this.showAlert = true;
+        } else {
+          $("#modalNaoEncontrado").modal("show");
+        }
+      });
   }
 
+  removeFiltroAlerta() {
+    this.loadDevices();
+    this.searchModelo = "";
+  }
 }
