7dfc7f24733d44b3a064346bb62a8f62099626fa
Renan Rodrigues Ramos <renan.ramos@inatel.br>
2021-02-28 01:00:44 -0300

    Removidos imports nao utilizados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,25 +9,20 @@ import { AppComponent } from './app.component';
 import { DeviceModule } from './device/device.module';
 import { HttpClientModule } from '@angular/common/http';
 import { AddButtonComponent } from './device/shared/components/add-button/add-button.component';
-import { DialogAlertComponent } from './device/shared/alert/dialog-alert/dialog-alert.component';
+import { DialogAlertModule } from './device/shared/alert/alert-modal.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AddButtonComponent,
-    DialogAlertComponent
-  ],
-  imports: [
-    BrowserModule,
-    BsDropdownModule.forRoot(),
-    TooltipModule.forRoot(),
-    ModalModule.forRoot(),
-    AppRoutingModule,
-    DeviceModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
-  entryComponents: [DialogAlertComponent]
+	declarations: [AppComponent, AddButtonComponent],
+	imports: [
+		BrowserModule,
+		BsDropdownModule.forRoot(),
+		TooltipModule.forRoot(),
+		ModalModule.forRoot(),
+		AppRoutingModule,
+		DeviceModule,
+		HttpClientModule,
+		DialogAlertModule
+	],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
