3c143784c771f5e324e6069de618cb5a07b248bc
Renan Rodrigues Ramos <renan.ramos@inatel.br>
2021-02-28 01:00:20 -0300

    Instacao e importacao de configuracoes do modulo ng-mask

--- src/app/device/device.module.ts
+++ src/app/device/device.module.ts
@@ -4,19 +4,23 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { DeviceListComponent } from './device-list/device-list.component';
 import { DeviceFormComponent } from './device-form/device-form.component';
+import { SpanErrorComponent } from './shared/components/span-error/span-error.component';
+import { IConfig, NgxMaskModule } from 'ngx-mask';
+import { DeviceSearchComponent } from './device-search/device-search.component';
 
-const routes: Routes = [
-  { path: 'novo', component: DeviceFormComponent }
-];
+const routes: Routes = [{ path: 'novo', component: DeviceFormComponent }];
+
+export const options: Partial<IConfig> | (() => Partial<IConfig>) = null;
 
 @NgModule({
-  declarations: [DeviceListComponent, DeviceFormComponent],
-  imports: [
-    CommonModule,
-    RouterModule.forRoot(routes),
-    FormsModule,
-    ReactiveFormsModule
-  ],
-  exports: [RouterModule]
+	declarations: [DeviceListComponent, DeviceFormComponent, SpanErrorComponent, DeviceSearchComponent],
+	imports: [
+		CommonModule,
+		RouterModule.forRoot(routes),
+		FormsModule,
+		ReactiveFormsModule,
+		NgxMaskModule.forRoot()
+	],
+	exports: [RouterModule]
 })
-export class DeviceModule { }
+export class DeviceModule {}
