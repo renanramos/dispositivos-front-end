45f42f7cbf757fcb81f484a5571239d777e98d2c
Renan Rodrigues Ramos <renan.ramos@inatel.br>
2021-02-28 00:56:47 -0300

    Melhorias no botao de inclusao de dispositivos

--- src/app/device/shared/components/add-button/add-button.component.ts
+++ src/app/device/shared/components/add-button/add-button.component.ts
@@ -1,24 +1,22 @@
-import { Component, OnInit, TemplateRef } from "@angular/core";
-import { Router } from "@angular/router";
-import { BsModalService } from "ngx-bootstrap/modal";
-import { BsModalRef } from "ngx-bootstrap/modal";
-import { DeviceFormComponent } from "src/app/device/device-form/device-form.component";
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { DeviceFormComponent } from 'src/app/device/device-form/device-form.component';
 
 @Component({
-  selector: "app-add-button",
-  templateUrl: "./add-button.component.html",
-  styleUrls: ["./add-button.component.css"],
+	selector: 'device-add-button',
+	templateUrl: './add-button.component.html',
+	styleUrls: ['./add-button.component.css']
 })
 export class AddButtonComponent implements OnInit {
-  constructor(private modalService: BsModalService) {}
+	constructor(private modalService: BsModalService) {}
 
-  ngOnInit() {}
+	ngOnInit() {}
 
-  addNewDevice() {
-    this.modalService.show(DeviceFormComponent, {
-      id: 1,
-      animated: true,
-      ignoreBackdropClick: true,
-    });
-  }
+	addNewDevice() {
+		this.modalService.show(DeviceFormComponent, {
+			id: 1,
+			animated: true,
+			ignoreBackdropClick: true
+		});
+	}
 }
