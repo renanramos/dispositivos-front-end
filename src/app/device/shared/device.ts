a4959f0decc960e0dd872d56692b2fd55162bbe6
Renan Rodrigues Ramos <renan.ramos@inatel.br>
2021-02-28 00:59:27 -0300

    Alterado tipo de campo capacidade de armazenamento

--- src/app/device/shared/device.ts
+++ src/app/device/shared/device.ts
@@ -1,9 +1,8 @@
 export class Device {
-
-    public device_id: number;
-    public device_modelo: string;
-    public device_fabricante: string;
-    public device_capacidade_armazenamento: string;
-    public device_tamanho_tela: number;
-    public device_versao_so: string;
+	public device_id: number;
+	public device_modelo: string;
+	public device_fabricante: string;
+	public device_capacidade_armazenamento: number;
+	public device_tamanho_tela: number;
+	public device_versao_so: string;
 }
